fba8fe601da13875ab9ac16058b9be9fe1492234
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-01-14 04:34:31 +0200

    add about section

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,15 @@ $(document).ready(()=>{
     $(".navbar-nav .nav-item .nav-link").click(function(){
         $(this).next().toggleClass("active").parent().siblings().find(".drop-down-menu.active").removeClass("active");
     })
+    // $(window).scroll(()=>{
+    //    const top= $(this).scrollTop(); 
+    //    console.log(top);
+    //    if(top>50)
+    //    {
+    //     $(".navbar").toggleClass("scrolled");
+    //    } 
+    // })
+    $(window).scroll(function(){
+        $(".navbar").toggleClass("scrolled" ,$(this).scrollTop() > 50)
+      })
 })
