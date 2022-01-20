00893df2b6fac9887f720099f60643687ad22980
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-01-20 20:51:46 +0200

    add project section

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,5 +19,29 @@ $(document).ready(()=>{
     // })
     $(window).scroll(function(){
         $(".navbar").toggleClass("scrolled" ,$(this).scrollTop() > 50)
-      })
+      });
+
+    
+    $('.project .owl-carousel').owlCarousel({
+        
+        loop:false,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+               
+            }
+        }
+    })
 })
