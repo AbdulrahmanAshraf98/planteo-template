5db13aa66ed32531d0c5bbf4ba01236200695fee
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-01-22 02:44:04 +0200

    add team member section

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,5 +44,28 @@ $(document).ready(()=>{
                
             }
         }
-    })
+    });
+    $('.team .owl-carousel').owlCarousel({
+        
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+               
+            }
+        }
+    });
 })
