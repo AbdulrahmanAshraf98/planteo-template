78cf3e1d56e22f7d2533f12a359669831995eefb
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-01-23 07:36:38 +0200

    brand  section

--- assets/js/app.js
+++ assets/js/app.js
@@ -107,4 +107,24 @@ $(document).ready(()=>{
             }
         }
     });
+    $('.brand .owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    });
 })
